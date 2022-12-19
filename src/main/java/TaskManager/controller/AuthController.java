867d1d62d85a879bb308bd2259c439de5853a62b
SarayShlomi <saraysara1996@gmail.com>
2022-12-19 18:09:47 +0200

    OAuth2 finctions

--- src/main/java/TaskManager/controller/AuthController.java
+++ src/main/java/TaskManager/controller/AuthController.java
@@ -1,9 +1,6 @@
 package TaskManager.controller;
 
-import TaskManager.entities.LoginData;
-import TaskManager.entities.LoginRequest;
-import TaskManager.entities.SecurityUser;
-import TaskManager.entities.User;
+import TaskManager.entities.*;
 import TaskManager.entities.responseEntities.UserDTO;
 import TaskManager.service.AuthService;
 import TaskManager.utils.emailUtils.EmailActivationFacade;
@@ -11,17 +8,21 @@ import TaskManager.utils.jwtUtils.JWTTokenHelper;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
 
 
 @RestController
@@ -38,6 +39,83 @@ public class AuthController {
     private final AuthenticationManager authenticationManager;
 
     private final JWTTokenHelper jWTTokenHelper;
+
+    //TODO: localhost:8080/auth/getDetails
+    //TODO: CHECK'S
+    //TODO: CHECK IF THIS ID THE FIRST TIME THE USER LOGIN THE APPLICATION OR NOT.
+    //TODO: found the email + name of the user
+
+
+
+    @RequestMapping(method = RequestMethod.POST, path = "/getToken")
+    public String getTokenFromCode(@RequestParam String code) {
+
+        //TODO: the first request from the frontend.. (1)
+        //TODO: localhost:8080/auth/getToken?code= (2)
+
+        String client_id = "f21614ae68732a9a2cc0";
+        String client_secret = "a62b7f0991686edd80a37b4b2aa63411df873fb0";
+        String Link = "https://github.com/login/oauth/access_token?";
+
+        String linkGetToken = Link + "client_id=" + client_id + "&client_secret=" + client_secret + "&code=" + code;
+        ResponseEntity<TokenResponse> result = getTokenFromCodeFunction(linkGetToken);
+
+        return result.getBody().getAccess_token();
+    }
+    public static ResponseEntity<TokenResponse> getTokenFromCodeFunction(String link) {  //send link return response
+        ResponseEntity<TokenResponse> response = null;
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", "application/json");
+        HttpEntity<String> entity = new HttpEntity<>(null, headers);
+        try {
+            return restTemplate.exchange(link, HttpMethod.POST, entity, TokenResponse.class);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Something went wrong!!");
+        }
+    }
+    @RequestMapping(method = RequestMethod.GET, path = "/getDetails")
+    public ResponseEntity<GitUser> getDetailsFromToken(@RequestParam String token) {
+
+        System.out.println(token);
+
+        String linkForName = "https://api.github.com/user"; //out from function
+
+        return getDetailsFromTokenFunction(linkForName, token);
+
+//        GitUser gitUser = result.getBody();
+//        if(result.getBody().getEmail()==null) {
+//            GithubEmail[] githubEmail = reqFromGitTheEmail("https://api.github.com/user", token).getBody();
+//            for (GithubEmail gEmail: githubEmail) {
+//                if (gEmail.isPrimary()) {
+//                        return null;
+//                }
+//            ResponseEntity<GithubEmail> resultEmail = reqFromGitTheEmail("https://api.github.com/user/emails", token); //get out the name
+//            resultName.getBody().setEmail(resultEmail.getBody().getEmail());
+//            }
+//        }
+    }
+    public static ResponseEntity<GitUser> getDetailsFromTokenFunction(String link, String bearerToken) {
+
+        ResponseEntity<GitUser> response = null;
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + bearerToken);
+        HttpEntity<String> entity = new HttpEntity<>(null, headers);
+
+        try {
+//           response=restTemplate.exchange(link, HttpMethod.GET, entity, GitUser.class);
+            GitUser gitUser = restTemplate.exchange(link, HttpMethod.GET, entity, GitUser.class).getBody();
+            System.out.println(gitUser.getName());
+            System.out.println(gitUser.getEmail());
+
+            return  ResponseEntity.ok(gitUser);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+
 
     @PostMapping(value = "/login", consumes = "application/json", produces = "application/json")
     public ResponseEntity<LoginData> login(@RequestBody LoginRequest credentials) {
@@ -65,7 +143,7 @@ public class AuthController {
     }
 
     @GetMapping
-    public void onlyAdmin(){
+    public void onlyAdmin() {
         System.out.println("hi admin");
 
         emailActivationFacade.sendVerificationEmail("saraysara1996@gmail.com");
--- src/main/java/TaskManager/email/EmailNotification.java
+++ src/main/java/TaskManager/email/EmailNotification.java
@@ -5,24 +5,13 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
-//@Component
 public class EmailNotification {
     private static String to;
     private static final String from = "graychairs1982@gmail.com";
-    //    @Value("password")
-//    private String password;
     private static final Properties properties = System.getProperties();
 
     Session session = Session.getInstance(properties, new Authenticator() {
         protected PasswordAuthentication getPasswordAuthentication() {
-//            Properties properties = new Properties();
-//            try (InputStream inputStream = taskManager.pro.class.getResourceAsStream("/application.properties")) {
-//
-//                properties.load(inputStream);
-//
-//            } catch (IOException e) {
-//                e.printStackTrace(System.out);
-//            }
             System.out.println();
             return new PasswordAuthentication("apikey", properties.getProperty("spring.mail.password"));
         }
@@ -37,7 +26,6 @@ public class EmailNotification {
         properties.put("mail.smtp.socketFactory.fallback", "true");
         properties.put("spring.mail.password", "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");
         session.setDebug(true);
-        System.out.println("ssssssssssssssssss : " + properties.getProperty("spring.mail.password"));
         return new MimeMessage(session);
     }
 
