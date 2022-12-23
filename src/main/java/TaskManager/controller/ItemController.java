3b34d42e52828dad818d2cafe940ee110978304b
SarayShlomi <saraysara1996@gmail.com>
2022-12-23 15:26:04 +0200

    send notification in update and delete item

--- src/main/java/TaskManager/controller/ItemController.java
+++ src/main/java/TaskManager/controller/ItemController.java
@@ -2,8 +2,10 @@ package TaskManager.controller;
 
 import TaskManager.entities.Comment;
 import TaskManager.entities.Item;
+import TaskManager.entities.entitiesUtils.NotificationTypes;
 import TaskManager.entities.responseEntities.ItemDTO;
 import TaskManager.service.ItemService;
+import TaskManager.service.NotificationService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 public class ItemController {
 
     private final ItemService itemService;
+    private final NotificationService notificationService;
 
     @PostMapping(value = "/item-create", consumes = "application/json", produces = "application/json")
     public ResponseEntity<ItemDTO> addNewItem(@RequestBody Item newItem){
@@ -22,7 +25,9 @@ public class ItemController {
     }
     @PutMapping(value = "/item-update/{itemId}", consumes = "application/json", produces = "application/json")
     public ResponseEntity<ItemDTO> updateItem(@PathVariable("itemId") int itemId,@RequestBody Item updatedItem){
-        return new ResponseEntity<>(itemService.updateItem(itemId,updatedItem),HttpStatus.OK);
+        ResponseEntity<ItemDTO> result = new ResponseEntity<>(itemService.updateItem(itemId, updatedItem), HttpStatus.OK);
+        notificationService.ItemNotification(123 , NotificationTypes.ITEM_DATA_CHANGED );
+        return  result;
     }
 
 
@@ -32,8 +37,10 @@ public class ItemController {
     }
 
     @DeleteMapping(value = "/item-delete/{itemId}")
-    public void deleteItem(@PathVariable("itemId") int itemId){
+    public void deleteItem(@PathVariable("itemId") int itemId) {
         itemService.deleteItem(itemId);
+        //TODO: FOUND BOARD ID
+        notificationService.ItemNotification(123, NotificationTypes.ITEM_DELETED); //send message to all users that in this board with the item that deleted
     }
 
     @PostMapping(value = "/add-comment/{itemId}")
--- src/main/java/TaskManager/service/NotificationService.java
+++ src/main/java/TaskManager/service/NotificationService.java
@@ -1,17 +1,43 @@
 package TaskManager.service;
 
-import TaskManager.entities.entitiesUtils.NotificationTypes;
+import TaskManager.email.EmailNotification;
+import TaskManager.entities.Board;
+import TaskManager.entities.Item;
 import TaskManager.entities.User;
+import TaskManager.entities.entitiesUtils.NotificationTypes;
+import TaskManager.repository.BoardRepository;
+import TaskManager.repository.ItemRepository;
+import TaskManager.repository.UserRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+
+import static TaskManager.entities.entitiesUtils.NotificationTypes.ITEM_DELETED;
+
 @Service
+@AllArgsConstructor
 public class NotificationService {
-    public void emailNotification(User user, NotificationTypes notificationTypes) throws IllegalAccessException {
-    if(user.getNotificationTypes().contains(notificationTypes)){
-        //EmailNotification.sendEmailNotification(user.getEmail(),notificationTypes.toString());
+
+    private final UserRepository userRepository;
+    private final BoardRepository boardRepository;
+    private final ItemRepository itemRepository;
+
+    public void ItemNotification(int boardId, NotificationTypes notificationTypes) {
+        Board board = boardRepository.findById(boardId).orElseThrow(() -> new IllegalArgumentException("board not found"));
+        Set<User> result = board.getUsersOnBoard();
+        sendMailMassage(result, notificationTypes);
     }
-    else
-        throw new IllegalAccessException("user do not get email on this notification type");
+
+
+    private void sendMailMassage(Set<User> userSet, NotificationTypes notificationTypes) {
+        for(User user : userSet){
+            if (user.getNotificationTypes().contains(notificationTypes) && (user.isEmailNotification())) {
+                EmailNotification.sendEmailNotification(user.getEmail(), notificationTypes.toString());
+            } else
+                System.out.println("user don't get email on this notification type");
+        }
     }
+
 
 }
